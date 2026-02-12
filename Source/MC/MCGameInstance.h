5813dd0c647a3a9cdffb3c73a6291cca5b96ced4
ErosEx <119348430+ErosEx@users.noreply.github.com>
2026-02-12 21:30:12 +0100

    commit new

--- Source/MC/MCGameInstance.h
+++ Source/MC/MCGameInstance.h
@@ -3,7 +3,8 @@
 #include "Engine/GameInstance.h"
 #include "MCGameInstance.generated.h"
 
-UCLASS()
+UCLASS(Blueprintable, BlueprintType)
+
 class MC_API UMCGameInstance : public UGameInstance
 {
     GENERATED_BODY()
